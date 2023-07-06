3b822f05957842e9babff674fc629e9f524f06fa
Caio Lucas <lcaio1281@gmail.com>
2023-07-06 11:12:14 -0300

    customizando tema default do native base

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+
 import { Loading } from "@components/Loading";
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,14 +16,14 @@ export default function App() {
   });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {/* {fontsLoaded ? <View /> : <Loading />} */}
-      <Loading />
+      {fontsLoaded ? <View /> : <Loading />}
+      {/* <Loading /> */}
     </NativeBaseProvider>
   );
 }
