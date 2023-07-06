896e4b2d4b52b8d020a74e302f369a941fe891ff
Caio Lucas <lcaio1281@gmail.com>
2023-07-06 11:32:54 -0300

    imagem de background

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
 
 import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +23,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
       {/* <Loading /> */}
     </NativeBaseProvider>
   );
