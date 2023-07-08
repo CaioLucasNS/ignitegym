4b462de35f97acf14e253a6b5a0afff8d42ca991
Caio Lucas <lcaio1281@gmail.com>
2023-07-08 01:29:10 -0300

    criando componente Group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,14 @@
-import { HomeHeader } from "@components/HomeHeader";
 import { VStack } from "native-base";
+
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <Group name="costas" />
     </VStack>
   );
 }
