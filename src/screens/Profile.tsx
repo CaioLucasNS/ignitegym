af311bbfedd45e1025a1eac3e200daf4cfb3b39d
Caio Lucas <lcaio1281@gmail.com>
2023-07-10 11:21:24 -0300

    buscando infos do arquivo com expo file system

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -9,6 +9,7 @@ import {
   Heading,
 } from "native-base";
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -38,6 +39,11 @@ export function Profile() {
       }
 
       if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+        console.log("photoInfo: ", photoInfo);
+
         setUserPhoto(photoSelected.assets[0].uri);
       }
     } catch (error) {
