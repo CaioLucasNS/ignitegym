3e1b62aff61ef5b26515758930f06827d0c54783
Caio Lucas <lcaio1281@gmail.com>
2023-07-12 12:26:17 -0300

    tratando erros da api com interceptors e toasts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,31 +1,19 @@
 import axios from "axios";
 
+import { AppError } from "@utils/AppError";
+
 const api = axios.create({
   baseURL: "http://127.0.0.1:3333",
 });
 
-/**
-config - acesso a todas as informações da requisição / request
-error - ação caso algo der errado
-api.interceptors.request.use(
-  (config) => {
-    console.log("DADOS ENVIADOS => ", config.data);
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
- */
-
 api.interceptors.response.use(
-  (response) => {
-    console.log("INTERCEPTOR RESPONSE => ", response);
-    return response;
-  },
+  (response) => response,
   (error) => {
-    console.log("INTERCEPTOR RESPONSE ERROR => ", error);
-    return Promise.reject(error);
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message)); // erro vindo do back-end
+    } else {
+      return Promise.reject(error); // erro genérico
+    }
   }
 );
 
