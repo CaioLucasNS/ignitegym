d2e3c845899102b2cee77d812a4f3deae694b82b
Caio Lucas <lcaio1281@gmail.com>
2023-07-12 12:05:28 -0300

    axios exceptions

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -3,6 +3,8 @@ import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+
+import axios from "axios";
 import { api } from "@services/api";
 
 import LogoSvg from "@assets/logo.svg";
@@ -10,6 +12,7 @@ import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { Alert } from "react-native";
 
 type FormDataProps = {
   name: string;
@@ -47,8 +50,14 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await api.post("/users", { name, email, password });
-    console.log(response.data);
+    try {
+      const response = await api.post("/users", { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
   }
 
   return (
