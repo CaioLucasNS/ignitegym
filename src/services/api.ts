585e31661d02c14c8d8b01014bb01411ba87f1be
Caio Lucas <lcaio1281@gmail.com>
2023-07-12 12:12:45 -0300

    entendendo interceptors do axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,32 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: "http://127.0.0.1:3333",
 });
+
+/**
+config - acesso a todas as informações da requisição / request
+error - ação caso algo der errado
+api.interceptors.request.use(
+  (config) => {
+    console.log("DADOS ENVIADOS => ", config.data);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+ */
+
+api.interceptors.response.use(
+  (response) => {
+    console.log("INTERCEPTOR RESPONSE => ", response);
+    return response;
+  },
+  (error) => {
+    console.log("INTERCEPTOR RESPONSE ERROR => ", error);
+    return Promise.reject(error);
+  }
+);
+
+export { api };
