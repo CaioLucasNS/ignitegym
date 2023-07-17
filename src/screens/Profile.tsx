c5a304ea6bbadd5d0769f9cb26fcedaacc32438b
Caio Lucas <lcaio1281@gmail.com>
2023-07-17 14:31:25 -0300

    fazendo upload da imagem

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -115,11 +115,23 @@ export function Profile() {
           name: `${user.name.replace(" ", "-")}.${fileExtension}`.toLowerCase(),
           uri: photoSelected.assets[0].uri,
           type: `${photoSelected.assets[0].type}/${fileExtension}`,
-        };
+        } as any;
 
-        console.log(JSON.stringify(photoFile, null, 4));
-        // setUserPhoto(photoSelected.assets[0].uri);
+        const userPhotoUploadForm = new FormData();
+        userPhotoUploadForm.append("avatar", photoFile);
+
+        await api.patch("/users/avatar", userPhotoUploadForm, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
       }
+
+      toast.show({
+        title: "Foto atualizada!",
+        placement: "top",
+        bgColor: "green.500",
+      });
     } catch (error) {
       console.log(error);
     } finally {
